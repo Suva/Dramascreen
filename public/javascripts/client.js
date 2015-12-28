973cdbe9a4ceacf38dff93f311c8c6783ecc665e
Jaan Pullerits <clysuva@gmail.com>
2015-12-28 23:04:32 +0200

    Add dramascreen display to client

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -9,18 +9,6 @@ $(function(){
 
        var url = "/message/" + $("#level").val();
 
-       log("Sending POST message to " + url);
-       log("Message: " + JSON.stringify(msg));
-
-       $.post(url, msg, function(resp){
-           log("Got response: " + JSON.stringify(resp));
-
-       })
+       $.post(url, msg)
    });
-
-    function log(msg){
-        var scr = $("#screen");
-        scr.append($("<div>").text(msg));
-        scr.scrollTop(scr[0].scrollHeight);
-    }
 });
