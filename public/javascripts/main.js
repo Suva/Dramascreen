bf5d97ae2927defb8d3af5bb83c9c367ae7dc99b
Jaan Pullerits <clysuva@gmail.com>
2015-12-28 22:45:19 +0200

    Add youtube player. Make browser reload page when server restarts.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,6 +20,8 @@ $(function(){
         return i + "th";
     }
 
+    var player;
+
     var months = [
         "January",
         "February",
@@ -56,11 +58,48 @@ $(function(){
     var socket = io();
     var turnOffTimeout;
 
+    setupMouseHide();
+    initYoutubeApi();
+
+    function isYoutube(message) {
+        return message.message.match(/^https?:\/\/(www.)?youtube.com/)
+    }
+
+    var version = null;
+    socket.on("init", function(initVersion){
+        if(version){
+            if(version != initVersion) {
+                console.log("Server restarted, reloading page...");
+                window.location.reload();
+            }
+        } else {
+            console.log("Server setup with version " + initVersion)
+            version = initVersion;
+        }
+
+    });
 
     socket.on("message", function(message){
-        setScreenDisplay(message);
-        playSound(message);
-        initFlipbackTimeout();
+        if(isYoutube(message)){
+            var vId = message.message.match(/v=([A-Za-z0-9_-]+)/)[1];
+            if(vId && player){
+                player.loadVideoById(vId, 0, "large");
+                $(".back-video").show();
+                $(".back").hide();
+                $("#screen")
+                    .removeClass("error")
+                    .removeClass("notice")
+                    .removeClass("warning")
+                    .removeClass("info")
+                    .addClass("flipped")
+                    .addClass(message.type);
+            }
+
+        } else {
+            setScreenDisplay(message);
+            playSound(message);
+            initFlipbackTimeout();
+        }
     });
 
     function initFlipbackTimeout() {
@@ -79,9 +118,30 @@ $(function(){
         }
     }
 
+    function setupMouseHide() {
+        var mouseMoveTimeout = null;
+        $("body").mousemove(function () {
+            if (!mouseMoveTimeout) {
+                $("body").removeClass("mouseHidden");
+            } else {
+                clearTimeout(mouseMoveTimeout);
+            }
+            mouseMoveTimeout = setTimeout(function () {
+                $("body").addClass("mouseHidden");
+                mouseMoveTimeout = null;
+            }, 2000)
+        });
+    }
+
     function setScreenDisplay(message) {
+        if(player) {
+            player.stopVideo();
+            player.clearVideo();
+        }
         $(".title").text(message.title);
         $(".message").text(message.message);
+        $(".back").show();
+        $(".back-video").hide();
         $("#screen")
             .removeClass("error")
             .removeClass("notice")
@@ -89,6 +149,42 @@ $(function(){
             .removeClass("info")
             .addClass("flipped")
             .addClass(message.type);
+    }
+
+    function initYoutubeApi(){
+        // 2. This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        // 3. This function creates an <iframe> (and YouTube player)
+        //    after the API code downloads.
+
+        onYouTubeIframeAPIReady = function() {
+            console.log("Initializing youtube player");
+            player = new YT.Player('player', {
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
+        }
+
+        // 4. The API will call this function when the video player is ready.
+        function onPlayerReady(event) {
+            event.target.playVideo();
+        }
+
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.ENDED) {
+                $("#screen").removeClass("flipped");
+            }
+        }
+
     }
 
 });
--- routes/messages.js
+++ routes/messages.js
@@ -1,11 +1,19 @@
 var io = null;
+var version = null;
 
 module.exports = {
     init: function(sock){
+        version = Date.now();
         io = sock;
+        io.on('connection', function (socket) {
+            socket.emit('init', version);
+        });
+
     },
 
     sendMessage: function(title, message, type){
         io.emit('message', {title: title, message: message, type: type});
     }
+
+
 };
