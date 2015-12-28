1aa07e6a2b18a4da8fb5b69a851898143210e85e
Jaan Pullerits <clysuva@gmail.com>
2015-12-28 23:22:24 +0200

    Update client design.

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,13 +1,15 @@
 $(function(){
-   $("#send").click(function(ev){
+   $("button").click(function(ev){
        ev.preventDefault();
+
+       var level = $(ev.target).data("level");
 
        var msg = {
            title: $("#title").val(),
            message: $("#message").val()
        };
 
-       var url = "/message/" + $("#level").val();
+       var url = "/message/" + level;
 
        $.post(url, msg)
    });
