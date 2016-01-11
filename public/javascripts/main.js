ffa79da01e3a8dd76c2a7b6f68d470951e887d61
Jaan Pullerits <jaan.pullerits@myjar.com>
2016-01-11 12:02:59 +0200

    Revert "Add day of the week into the view."
    This reverts commit b7bea2eaa0712891a89970d09c6c9c9451569c13.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -52,11 +52,6 @@ $(function(){
             prevTime = timeString;
             $(".time").text(timeString);
             $(".date").text(ordinal_suffix_of(date.getDate()) + " " + months[date.getMonth()] + " " + date.getFullYear());
-            if(date.getDay() == 5){
-                $(".day").addClass("friday").text("FRIDAY");
-            } else {
-                $(".day").removeClass("friday").text("NOT FRIDAY");
-            }
         }
     });
 
